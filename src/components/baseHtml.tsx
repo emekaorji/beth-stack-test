d3113b46f0797e0a8c19e5140db9d20649d3eeab
Emeka Orji <emekapraiseo@gmail.com>
2023-10-04 05:20:11 +0100

    add style

--- src/components/baseHtml.tsx
+++ src/components/baseHtml.tsx
@@ -27,6 +27,7 @@ const BaseHtml = ({ children }: elements.Children) => `
 			width: 100vw;
 			height: 100vh;
 			display: flex;
+			gap: 1em;
 			flex-direction: column;
 			justify-content: center;
 			align-items: center;
